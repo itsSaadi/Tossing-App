7fa5a7db4e6eb282c1a7841f21309a9c0de5850e
Saad Ishfaq <sishfaq984@gmail.com>
2023-11-21 15:43:47 +0500

    Toss Logic Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const tossHandle = () => {
+    let a = prompt("Select Heads or Tails");
+    if (a) {
+      const result = Math.floor(Math.random() * 2);
+      if (result === 0) {
+        alert("Winner is Heads");
+      } else {
+        alert("Winner is Tails");
+      }
+    } else {
+      alert("Please Select Heads or tails");
+    }
+  };
   return (
-    <div className="App">
-      Welcome to Tossing app
-    </div>
+    <>
+      <div className="container my-4">
+        <h1>Welcome to Tossing app</h1>
+        <button className="btn btn-primary" onClick={tossHandle}>
+          Toss
+        </button>
+      </div>
+    </>
   );
 }
 
